5fafbcb2df7c316a7374e8e5d563cf0f77c4e0fb
Luizgsch <luizgsch1@gmail.com>
2026-03-26 11:02:40 -0300

    ajustes no package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sistema_Jornada/", // OBRIGATÓRIO para GitHub Pages
+  base: "/Sistema_Jornada/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
